46e13c51b1919774ccb7e4d6f519a09fdff29d36
elliotGaulin <elliotgaulin@hotmail.com>
2022-11-02 14:51:18 -0400

    ajout de la table dans les models

--- ebadge_laravel/app/Models/User.php
+++ ebadge_laravel/app/Models/User.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'user';
 }
--- ebadge_laravel/app/Models/UserBadge.php
+++ ebadge_laravel/app/Models/UserBadge.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class UserBadge extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_badge';
 }
