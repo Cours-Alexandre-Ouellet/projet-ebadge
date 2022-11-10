84a4515759dcfde73587644fa21a54cc1310200e
Raphael <raphboisvert@live.fr>
2022-11-10 13:23:31 -0500

    placements des éléments UI

--- ebadge_React/src/App.js
+++ ebadge_React/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from './composant/Test';
+import Login from './composant/Login';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3949B5',
+    },
+    secondary: {
+      main: '#FAC710',
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +34,8 @@ function App() {
         </a>
       </header>
       <Test />
+      <Login />
+      </ThemeProvider>
     </div>
   );
 }
--- ebadge_React/src/composant/Login.js
+++ ebadge_React/src/composant/Login.js
@@ -1,10 +1,85 @@
 import React from 'react';
+import './Login.css';
+import '@mui/material';
+import { Button, TextField } from '@mui/material';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            identifier: '',
+            password: '',
+            identifierValid: false,
+            passwordValid: false,
+            submitDisabled: true
+        }
+    }
+
+    handleChangeIdentifier(e) {
+        let identifierValid = e.target.value ? true : false; 
+        let submitValid = this.state.textValid && identifierValid;
+        this.setState({
+            identifier: e.target.value,
+            identifierValid: identifierValid,
+            identifierDisabled: !submitValid
+        })
+    }
+
+    handleChangePassword(e) {
+        let passwordValid = e.target.value ? true : false;
+        let submitValid = this.state.emailValid && passwordValid;
+        this.setState({
+            password: '',
+            passwordValid: passwordValid,
+            submitDisabled: !submitValid
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="login">
+                <div className="login-container">
+                    <div className="login-background">
+                    </div>
+                    <div className="login-right">
+                        <div className="login-right-content">
+                            <h1>E-Badge</h1>
+                            <form>
+                                <TextField
+                                    id="identifier"
+                                    label="Identifiant"
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    onChange={this.handleChangeIdentifier.bind(this)}
+                                    value={this.state.identifier}
+                                    sx={{ width: '100%' }}
+                                />
+                                <TextField
+                                    id="password"
+                                    label="Mot de passe"
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    onChange={this.handleChangePassword.bind(this)}
+                                    value={this.state.password}
+                                    sx={{ width: '100%' }}
+                                />
+                                <Button
+                                    variant="contained"
+                                    size="Large"
+                                    enable={this.state.submitDisabled}
+                                    sx={{
+                                        width: '60%',
+                                        marginTop: '20px'
+                                    }}>
+                                    Connexion →
+                                </Button>
+                            </form>
+                            <p><a href='/'>Mot de passe oublié?</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
