bcd10994e76bd6af03834896348fa1d1ae6fa3f5
Elliot Gaulin <2039854@carrefour.cegepvicto.ca>
2022-11-23 14:09:16 -0500

    aaah

--- ebadge_React/src/App.js
+++ ebadge_React/src/App.js
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Test from './composant/Test';
 import Layout from "./pages/Layout";
 import Login from "./pages/Login/Login";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PageProfil from "./composant/PageProfil";
 
 const theme = createTheme({
   palette: {
@@ -24,7 +24,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Test />} />
+            <Route index element={<PageProfil />} />
             <Route path="login" element={<Login />} />
           </Route>
         </Routes>
