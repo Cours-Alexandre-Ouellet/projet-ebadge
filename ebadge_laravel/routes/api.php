4406e72d9959068c1f636a4dfe500c5ed12a95cf
Elliot Gaulin <2039854@carrefour.cegepvicto.ca>
2022-11-28 15:46:38 -0500

    suppression d'un commentaire inutile

--- ebadge_laravel/routes/api.php
+++ ebadge_laravel/routes/api.php
@@ -5,16 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 Route::group([
     'prefix' => 'teacher-code',
     'middleware' => [
