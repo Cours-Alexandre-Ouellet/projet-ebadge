1ffb51a0bbf4aa843cee4b85e116dc4a2831f588
Raphael <raphboisvert@live.fr>
2022-11-30 19:33:12 -0500

    appel a l'api, meilleure selection des couleurs

--- ebadge_React/src/composant/BadgeCreateForm.js
+++ ebadge_React/src/composant/BadgeCreateForm.js
@@ -1,36 +1,45 @@
 import React from 'react';
 import './BadgeCreateForm.css';
 import '@mui/material';
-import { Button, TextField, InputAdornment } from '@mui/material';
+import { Button, TextField, InputAdornment, Autocomplete } from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
+import Api from '../utils/Api';
+import BadgeComposant from './BadgeComponent';
 
 class BadgeCreateForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: '',
-            description: '',
-            image: '',
-            color: '',
             titleError: '',
-            descriptionError: ''
+            descriptionError: '',
+            colorAutoComplete: null,
+            badge: {
+                title: '',
+                description: '',
+                imagePath: '',
+                color: '',
+                pourcentage: 0
+            }
         }
 
+        this.handleBadgeChange = this.handleBadgeChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.validateTitle = this.validateTitle.bind(this);
         this.validateDescription = this.validateDescription.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleBadgeChange(event) {
+        // change state of badge.name
+        this.setState({ badge: { ...this.state.badge, [event.target.name]: event.target.value } });
+    }
+
     handleChange(event) {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
+        this.setState({ [event.target.name]: event.target.value });
     }
 
     validateTitle() {
-        if (this.state.title.length === 0) {
+        if (this.state.badge.title.length === 0) {
             this.setState({ titleError: 'Veuillez renseigner le titre' });
             return false;
         } else {
@@ -40,7 +49,7 @@ class BadgeCreateForm extends React.Component {
     }
 
     validateDescription() {
-        if (this.state.description.length === 0) {
+        if (this.state.badge.description.length === 0) {
             this.setState({ descriptionError: 'Veuillez renseigner la description' });
             return false;
         } else {
@@ -53,6 +62,20 @@ class BadgeCreateForm extends React.Component {
         event.preventDefault();
         if (this.validateTitle() && this.validateDescription()) {
             // TODO: Appel API
+            Api.post('/badge', {
+                title: this.state.badge.title,
+                description: this.state.badge.description,
+                image: this.state.badge.image,
+                color: this.state.badge.color
+            })
+                .then((response) => {
+                    console.log(response);
+                }
+                )
+                .catch((error) => {
+                    console.log(error);
+                }
+                );
         }
     }
 
@@ -69,8 +92,8 @@ class BadgeCreateForm extends React.Component {
                                     name="title"
                                     label="Titre"
                                     variant="outlined"
-                                    value={this.state.title}
-                                    onChange={this.handleChange}
+                                    value={this.state.badge.title}
+                                    onChange={this.handleBadgeChange}
                                     onBlur={this.validateTitle}
                                     error={this.state.titleError.length > 0}
                                     helperText={this.state.titleError}
@@ -82,8 +105,8 @@ class BadgeCreateForm extends React.Component {
                                     name="description"
                                     label="Description"
                                     variant="outlined"
-                                    value={this.state.description}
-                                    onChange={this.handleChange}
+                                    value={this.state.badge.description}
+                                    onChange={this.handleBadgeChange}
                                     onBlur={this.validateDescription}
                                     error={this.state.descriptionError.length > 0}
                                     helperText={this.state.descriptionError}
@@ -96,8 +119,8 @@ class BadgeCreateForm extends React.Component {
                                         color='secondary'
                                         id='image'
                                         name='image'
-                                        value={this.state.image}
-                                        onChange={this.handleChange}
+                                        value={this.state.badge.imagePath}
+                                        onChange={this.handleBadgeChange}
                                         sx={{
                                             width: '100%',
                                             marginTop: '20px',
@@ -112,36 +135,46 @@ class BadgeCreateForm extends React.Component {
                                             hidden
                                         />
                                     </Button>
-                                    <TextField
+                                    <Autocomplete
                                         id="color"
                                         name="color"
-                                        label="Hex couleur background"
-                                        variant="standard"
-                                        value={this.state.color}
-                                        onChange={this.handleChange}
-                                        sx={{
-                                            width: '100%',
-                                            marginTop: '20px'
+                                        options={colors}
+                                        inputValue={this.state.badge.color}
+                                        onInputChange={(event, newInputValue) => {
+                                            this.setState({ badge: { ...this.state.badge, color: newInputValue } });
                                         }}
-                                        InputProps={{
+                                        getOptionLabel={(option) => option}
+                                        selectOnFocus
+                                        handleHomeEndKeys
+                                        freeSolo
+                                        sx={{ width: '100%', marginTop: '20px' }}
+                                        renderOption={(props, option) => (
+                                            <li {...props}>
+                                                <div
+                                                    className="badge-create-form-color"
+                                                    style={{ backgroundColor: "#" + option, width: '20px', height: '20px' }}
+                                                />
+                                                #{option}
+                                            </li>
+                                        )}
+                                        renderInput={(params) => <TextField {...params} label="Hex couleur arrière-plan" variant='standard' InputProps={{
+                                            ...params.InputProps,
                                             startAdornment: (
                                                 <InputAdornment position="start">
+                                                    <div
+                                                        className="badge-create-form-color"
+                                                        style={{ backgroundColor: "#" + this.state.badge.color, width: '20px', height: '20px' }}
+                                                    />
                                                     #
                                                 </InputAdornment>
                                             ),
-                                            endAdornment: (
-                                                <InputAdornment position="end">
-                                                    <div
-                                                        className="badge-create-form-color"
-                                                        style={{ backgroundColor: "#" + this.state.color, width: '20px', height: '20px' }}
-                                                    />
-                                                </InputAdornment>
-                                            ),
-                                        }}
+                                        }} />}
+
+
                                     />
                                 </div>
                                 <div className="badge-create-form-button-submit">
-                                    <Button variant="outlined" sx={{
+                                    <Button variant="outlined" onClick={this.props.handleClose} sx={{
                                         width: '100%',
                                         marginTop: '20px',
                                         marginRight: '20px'
@@ -156,15 +189,7 @@ class BadgeCreateForm extends React.Component {
                         <div className="badge-create-form-preview">
                             <h2>Prévisualisation</h2>
                             <div className="badge-create-form-preview-content">
-                                <div className="badge-create-form-preview-content-title">
-                                    <h2>{this.state.title}</h2>
-                                </div>
-                                <div className="badge-create-form-preview-content-description">
-                                    <p>{this.state.description}</p>
-                                </div>
-                                <div className="badge-create-form-preview-content-image">
-                                    <img src={this.state.image} alt="Badge" />
-                                </div>
+                                <BadgeComposant badge={this.state.badge}></BadgeComposant>
                             </div>
                         </div>
                     </div>
@@ -173,5 +198,16 @@ class BadgeCreateForm extends React.Component {
         );
     }
 }
+
+const colors = [
+    'ff0000',
+    'ff8000',
+    'ffff00',
+    '80ff00',
+    '00ff00',
+    '00ff80',
+    '00ffff',
+    '0080ff',
+];
 
 export default BadgeCreateForm;
