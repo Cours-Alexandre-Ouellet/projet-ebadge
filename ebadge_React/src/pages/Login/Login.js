7f70182702e9f420da20913544119406b31a117a
Elliot Gaulin <2039854@carrefour.cegepvicto.ca>
2022-12-13 11:34:35 -0500

    ajout du loading et vérification que les validations sont correctes

--- ebadge_React/src/pages/Login/Login.js
+++ ebadge_React/src/pages/Login/Login.js
@@ -5,6 +5,7 @@ import { Button, TextField } from '@mui/material';
 import Api from '../../utils/Api';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
+import Loading from '../../composant/Loading/LoadingComponent';
 class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -14,6 +15,7 @@ class Login extends React.Component {
             identifierError: '',
             passwordError: '',
             redirect: false,
+            isLoading: false
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -56,14 +58,16 @@ class Login extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault();
         if (this.validateIdentifier() && this.validatePassword()) {
+            this.setState({ isLoading: true });
             Api.post('/auth/login', {
                 email: this.state.identifier,
                 password: this.state.password
             }).then((response) => {
                 localStorage.setItem('token', response.data.access_token);
                 localStorage.setItem('username', response.data.username);
-                this.setState({ redirect: true });
+                this.setState({ redirect: true, isLoading: false });
             }).catch((error) => {
+                this.setState({isLoading: false})
                 if (error.response) {
                     switch (error.response.status) {
                         case 401:
@@ -104,6 +108,7 @@ class Login extends React.Component {
         }
         return (
             <div className="login">
+                {this.state.isLoading ? <Loading></Loading> : null}
                 <div className="login-container">
                     <div className="login-background">
                     </div>
