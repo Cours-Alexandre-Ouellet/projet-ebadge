51441229a67c7048e42cc7efbdc089c5433d3f52
Jonathan Côté <55141844+snakeeyes1023@users.noreply.github.com>
2022-12-16 15:03:15 -0500

    Ajout de certains tests dans badgeTest

--- ebadge_laravel/tests/Feature/BadgeTest.php
+++ ebadge_laravel/tests/Feature/BadgeTest.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Role;
+use Carbon\Carbon;
+use App\Models\User;
 
 
 class BadgeTest extends TestCase
@@ -20,9 +23,37 @@ class BadgeTest extends TestCase
         parent::setUp();
 
         $this->user = factory(\App\Models\User::class)->create();
-        $this->badge = factory(\App\Models\Badge::class)->create();
 
-        $this->token = $this->user->createToken('Personal Access Token')->accessToken;
+        $this->admin = factory(\App\Models\User::class)->create();
+        $this->admin->role_id = Role::Admin()->id;
+        $this->admin->save();
+
+
+        $this->teacher = factory(\App\Models\User::class)->create();
+        $this->teacher->role_id = Role::Teacher()->id;
+        $this->teacher->save();
+
+        $this->badge = factory(\App\Models\Badge::class)->create();
+        $this->badge->save();
+
+
+        $token = $this->user->createToken('Personal Access Token');
+        $token->token->expires_at = Carbon::now()->addWeeks(1);
+        $token->token->save();
+        $token->expires_at = now()->addMinutes(30);
+        $this->userToken = $token->accessToken;
+
+        $token = $this->teacher->createToken('Personal Access Token');
+        $token->token->expires_at = Carbon::now()->addWeeks(1);
+        $token->token->save();
+        $token->expires_at = now()->addMinutes(30);
+        $this->teacherToken = $token->accessToken;
+
+        $token = $this->admin->createToken('Personal Access Token');
+        $token->token->expires_at = Carbon::now()->addWeeks(1);
+        $token->token->save();
+        $token->expires_at = now()->addMinutes(30);
+        $this->adminToken = $token->accessToken;
     }
 
     /**
@@ -30,7 +61,7 @@ class BadgeTest extends TestCase
      */
     public function testCreateBadgeWithoutRequiredFields(): void
     {
-        $response = $this->post('/api/badge/create', ['Authorization' => 'Bearer ' . $this->token], [
+        $response = $this->post('/api/badge/create', ['Authorization' => 'Bearer ' . $this->teacherToken], [
             'title' => $this->badge->title,
             'description' => $this->badge->description,
         ]);
@@ -39,44 +70,96 @@ class BadgeTest extends TestCase
     }
 
     /**
-     * ○Scénario 2 : Suppression d’un badge alors qu’il est lié à des étudiants
+     * Créer un badge avec tous les champs obligatoires
      */
-    /*
-    public function testDeleteBadge(): void
+    public function testCreateBadgeWithRequiredFields(): void
     {
-        $response = $this->delete('/api/badge/'. $this->badge->id, ['Authorization' => 'Bearer ' . $this->token]);
+        $response = $this->post('/api/badge/create', [
+            'title' => $this->badge->title,
+            'description' => $this->badge->description,
+            'image' => $this->badge->image,
+        ], ['Authorization' => 'Bearer ' . $this->teacherToken]);
 
         $response->assertStatus(200);
     }
-    */
+
+    /**
+     * ○Scénario 2 : Suppression d’un badge alors qu’il est lié à des étudiants
+     */
+    public function testDeleteBadge(): void
+    {
+        $response = $this->delete('/api/badge/' . $this->badge->id, ['Authorization' => 'Bearer ' . $this->adminToken]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing('badges', [
+            'id' => $this->badge->id,
+        ]);
+    }
+
 
     /**
      * ○Scénario 3 : Modification d’un badge sans certains champs obligatoires
      */
-
     public function testUpdateBadgeWithoutRequiredFields(): void
     {
-        $response = $this->put('/api/badge', [
+        $response = $this->post('/api/badge', [
+            "id" => $this->badge->id,
+            "title" => $this->badge->title . "Test",
+        ], ['Authorization' => 'Bearer ' . $this->teacherToken]);
+
+        $response->assertStatus(405);
+
+
+        //check if the badge has not been updated
+        $this->assertDatabaseHas('badges', [
             'id' => $this->badge->id,
             'title' => $this->badge->title,
-            'description' => $this->badge->description,
-        ], ['Authorization' => 'Bearer ' . $this->token]);
-
-        $response->assertStatus(422);
+        ]);
     }
-
 
     /**
      * ○Scénario 4 : Récupérer un badge retourne bien tous les champs visibles du badge
      */
-    /*
     public function testGetBadge()
     {
-        $response = $this->get('/api/badge/get', [
-            'id' => $this->badge->id,
-        ]);
+        $response = $this->get('/api/badge', ['Authorization' => 'Bearer ' . $this->teacherToken]);
 
         $response->assertStatus(200);
     }
-    */
+
+    /**
+     * Il est impossible de récuperer tous les bagdes sans être connecté
+     */
+    public function testGetAllBadgesWithoutToken()
+    {
+        $response = $this->get('/api/badge');
+
+        $response->assertStatus(401);
+
+        $response->assertJson([
+            'message' => 'Unauthenticated.',
+        ]);
+    }
+
+    /**
+     * Modfiier un badge avec tous les champs obligatoires
+     */
+    public function testUpdateBadgeWithRequiredFields(): void
+    {
+        $response = $this->post('/api/badge', [
+            "id" => $this->badge->id,
+            "title" => $this->badge->title . "test",
+            "description" => $this->badge->description,
+            "image" => $this->badge->image,
+        ], ['Authorization' => 'Bearer ' . $this->teacherToken]);
+
+        $response->assertStatus(200);
+
+        //check if the badge has been updated
+        $this->assertDatabaseHas('badges', [
+            'id' => $this->badge->id,
+            'title' => $this->badge->title . "test",
+        ]);
+    }
 }
--- ebadge_laravel/tests/Feature/StatTest.php
+++ ebadge_laravel/tests/Feature/StatTest.php
@@ -5,46 +5,64 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Role;
+use Carbon\Carbon;
+
 
 class StatTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * SETUP
      */
     public function setUp(): void
     {
         parent::setUp();
-
         $this->user = factory(\App\Models\User::class)->create();
+
+        $this->admin = factory(\App\Models\User::class)->create();
+        $this->admin->role_id = Role::Admin()->id;
+        $this->admin->save();
+
+
+        $this->teacher = factory(\App\Models\User::class)->create();
+        $this->teacher->role_id = Role::Teacher()->id;
+        $this->teacher->save();
         $this->badge = factory(\App\Models\Badge::class)->create();
 
-        $this->roleStudent = factory(\App\Models\Role::class)->create([
-            'name' => 'Étudiant',
-        ]);
 
-        $this->student = factory(\App\Models\User::class)->create(
-            [
-                'role_id' => $this->roleStudent->id,
-                'is_private' => false,
-            ]
-        );
+        $token = $this->user->createToken('Personal Access Token');
+        $token->token->expires_at = Carbon::now()->addWeeks(1);
+        $token->token->save();
+        $token->expires_at = now()->addMinutes(30);
+        $this->userToken = $token->accessToken;
+
+        $token = $this->teacher->createToken('Personal Access Token');
+        $token->token->expires_at = Carbon::now()->addWeeks(1);
+        $token->token->save();
+        $token->expires_at = now()->addMinutes(30);
+        $this->teacherToken = $token->accessToken;
+
+        $token = $this->admin->createToken('Personal Access Token');
+        $token->token->expires_at = Carbon::now()->addWeeks(1);
+        $token->token->save();
+        $token->expires_at = now()->addMinutes(30);
+        $this->adminToken = $token->accessToken;
     }
 
     /**
      * ○Scénario 1 : Affichage du classement des étudiants
      */
     public function testLeaderboard(){
-        $response = $this->get('/api/stats/leaderboard');
+        $response = $this->get('/api/stats/leaderboard', ['Authorization' => 'Bearer ' . $this->adminToken]);
         $response->assertStatus(200);
     }
     /**
      * Le leader board n'affiche pas les étudiant qui sont privé
      */
     public function testLeaderboardPrivate(){
-        $response = $this->get('/api/stats/leaderboard');
+        $response = $this->get('/api/stats/leaderboard', ['Authorization' => 'Bearer ' . $this->adminToken]);
         $response->assertStatus(200);
-
-        // verifie que l'usager
     }
-
 }
