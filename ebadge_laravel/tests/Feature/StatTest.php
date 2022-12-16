63426a12abe080382401e23649fa71fbe8dff04b
Jonathan Côté <55141844+snakeeyes1023@users.noreply.github.com>
2022-12-16 10:51:15 -0500

    ajout des authorisations

--- ebadge_laravel/tests/Feature/StatTest.php
+++ ebadge_laravel/tests/Feature/StatTest.php
@@ -17,11 +17,34 @@ class StatTest extends TestCase
 
         $this->user = factory(\App\Models\User::class)->create();
         $this->badge = factory(\App\Models\Badge::class)->create();
+
+        $this->roleStudent = factory(\App\Models\Role::class)->create([
+            'name' => 'Étudiant',
+        ]);
+
+        $this->student = factory(\App\Models\User::class)->create(
+            [
+                'role_id' => $this->roleStudent->id,
+                'is_private' => false,
+            ]
+        );
     }
 
+    /**
+     * ○Scénario 1 : Affichage du classement des étudiants
+     */
     public function testLeaderboard(){
         $response = $this->get('/api/stats/leaderboard');
         $response->assertStatus(200);
+    }
+    /**
+     * Le leader board n'affiche pas les étudiant qui sont privé
+     */
+    public function testLeaderboardPrivate(){
+        $response = $this->get('/api/stats/leaderboard');
+        $response->assertStatus(200);
+
+        // verifie que l'usager
     }
 
 }
