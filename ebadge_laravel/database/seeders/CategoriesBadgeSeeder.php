c00bd91be60ef10553ca4c4a44c40e91262dae51
Elyas <ebenyssad@gmail.com>
2025-02-04 13:55:33 -0500

    Ajout des des changements sur la bd + donnees de base

---
+++ ebadge_laravel/database/seeders/CategoriesBadgeSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesBadgeSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        DB::table('categories_badge')->insert([
+            [
+                'idBadge' => 1,
+                'idCategorie' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idBadge' => 2,
+                'idCategorie' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idBadge' => 3,
+                'idCategorie' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idBadge' => 4,
+                'idCategorie' => 4,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idBadge' => 5,
+                'idCategorie' => 5,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+        ]);
+    }
+}
