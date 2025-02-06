cc72116b61c45ca00a0d3b8c20b08b74b6f75c22
pigeonsoldier <thomas.laliberte@icloud.com>
2025-02-06 17:27:18 -0500

    refactorisation du code de listeBadge en nouveau react et ajout du popup

--- ebadge_React/src/pages/ListeBadge.js
+++ ebadge_React/src/pages/ListeBadge.js
@@ -1,40 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ListeBadge.css";
 import "@mui/material";
-import {
-  TextField,
-  InputAdornment,
-} from "@mui/material";
+import { TextField, InputAdornment } from "@mui/material";
 import { Avatar } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import { DataGrid } from "@mui/x-data-grid";
 import Api from "../utils/Api";
+import BadgePopup from "../composant/Dashboard/BadgePopup";
 /**
- * Page affichant deux listes de badges une de badges obtenus et une de badges non obtenus. 
- * 
+ * Page affichant deux listes de badges une de badges obtenus et une de badges non obtenus.
+ *
  * le code à été en partie inspirer de Classement.js pour garder un theme constant dans le site.
  */
-class ListeBadge extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      closeBadgeForm: false,
-      /**
-       * initialisation des badges vide
-       */
-      badgesObtenus: [],
-      badgesNonObtenus: [],
-      search: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.filtrerBadgesObtenus = this.filtrerBadgesObtenus.bind(this);
-    this.filtrerBadgesNonObtenus = this.filtrerBadgesNonObtenus.bind(this);
-  }
+export default function ListeBadge() {
+  const [badgesObtenus, setBadgesObtenus] = useState([]);
+  const [badgesNonObtenus, setBadgesNonObtenus] = useState([]);
+  const [search, _] = useState("");
+  const [open, setOpen] = useState(false);
+  const [badgeSelectionne, setBadgeSelectionne] = useState(null);
 
   /**
    * fonction allant chercher tout les badges obtenues et non obtenues
    */
-  componentDidMount() {
+  useEffect(() => {
     let id = null;
     Api.get("/auth/current_user")
       .then((response) => {
@@ -42,8 +30,7 @@ class ListeBadge extends React.Component {
         Api.get("/user/" + id + "/badges")
           .then((response) => {
             const badges = response.data;
-            console.log(badges);
-            this.setState({ badgesObtenus: Object.values(badges).flat() });
+            setBadgesObtenus(Object.values(badges).flat());
           })
           .catch((error) => {
             console.log(error);
@@ -51,7 +38,7 @@ class ListeBadge extends React.Component {
         Api.get("/user/" + id + "/badges-left")
           .then((response) => {
             const badges = response.data;
-            this.setState({ badgesNonObtenus: Object.values(badges).flat() });
+            setBadgesNonObtenus( Object.values(badges).flat() );
           })
           .catch((error) => {
             console.log(error);
@@ -60,13 +47,13 @@ class ListeBadge extends React.Component {
       .catch((error) => {
         console.log(error);
       });
-  }
+  });
 
   /**
    * fonction qui permet de mettre à jour le state quand on change la valeur d'un champ
    * @param {*} event
    */
-  handleChange(event) {
+  function handleChange(event) {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
@@ -77,9 +64,9 @@ class ListeBadge extends React.Component {
    *  fonction qui permet de filtrer les badges obtenus par par leur nom
    * @returns les badges possedant le nom recherché
    */
-  filtrerBadgesObtenus() {
-    return this.state.badgesObtenus.filter((item) => {
-      return item.title.toLowerCase().includes(this.state.search.toLowerCase());
+  function filtrerBadgesObtenus() {
+    return badgesObtenus.filter((item) => {
+      return item.title.toLowerCase().includes(search.toLowerCase());
     });
   }
 
@@ -87,86 +74,103 @@ class ListeBadge extends React.Component {
    *  fonction qui permet de filtrer les badges non obtenus par par leur nom
    * @returns les badges possedant le nom recherché
    */
-  filtrerBadgesNonObtenus() {
-    return this.state.badgesNonObtenus.filter((item) => {
-      return item.title.toLowerCase().includes(this.state.search.toLowerCase());
+  function filtrerBadgesNonObtenus() {
+    return badgesNonObtenus.filter((item) => {
+      return item.title.toLowerCase().includes(search.toLowerCase());
     });
   }
 
-  render() {
-    let columns = [
-      { field: "title", headerName: "titre", width: 150 },
-      { field: "description", headerName: "description", width: 150 },
-      {
-        field: "imagePath",
-        headerName: "Image",
-        sortable: false,
-        flex: 2,
-        align: "center",
-        headerAlign: "center",
-        renderCell: (params) => {
-          return (
-            <Avatar
-              alt={params.row.title}
-              src={params.value}
-              sx={{ width: 40, height: 40 }}
-            />
-          );
-        },
+  /**
+   * Ferme la popup de gestion des badges
+   * @param {*} event
+   */
+  function handleClose() {
+    setOpen(false);
+  }
+
+  let columns = [
+    { field: "title", headerName: "titre", width: 150 },
+    { field: "description", headerName: "description", width: 150 },
+    {
+      field: "imagePath",
+      headerName: "Image",
+      sortable: false,
+      flex: 2,
+      align: "center",
+      renderCell: (params) => {
+        return (
+          <Avatar
+            alt={params.row.title}
+            src={params.value}
+            sx={{ width: 40, height: 40 }}
+          />
+        );
       },
-    ];
-    return (
-      <div className="listeBadge">
-        <div className="listeBadge-container">
-          <div className="listeBadge-background">
-            <div className="listeBadge-title">
-              <h1>Badges</h1>
+    },
+  ];
+
+  //
+  function handleRowClick(params){
+    setBadgeSelectionne(params.row);
+    setOpen(true);
+  };
+
+  return (
+    <div className="listeBadge">
+      <div className="listeBadge-container">
+        <div className="listeBadge-background">
+          <div className="listeBadge-title">
+            <h1>Badges</h1>
+          </div>
+          <div className="listeBadge-table">
+            <div className="listeBadge-table-header">
+              <TextField
+                id="search"
+                name="search"
+                label="Rechercher un nom"
+                variant="outlined"
+                value={search}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Search />
+                    </InputAdornment>
+                  ),
+                }}
+                sx={{ margin: "5px", width: "100%" }}
+              />
             </div>
-            <div className="listeBadge-table">
-              <div className="listeBadge-table-header">
-                <TextField
-                  id="search"
-                  name="search"
-                  label="Rechercher un nom"
-                  variant="outlined"
-                  value={this.state.search}
-                  onChange={this.handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <Search />
-                      </InputAdornment>
-                    ),
-                  }}
-                  sx={{ margin: "5px", width: "100%" }}
+            <div className="listeBadge-listes">
+              <div style={{ height: 400, width: "100%" }}>
+                <div className="listeBadge-title">
+                  <h2>Obtenus</h2>
+                </div>
+                <DataGrid
+                  onRowClick={handleRowClick}
+                  rows={filtrerBadgesObtenus()}
+                  columns={columns}
                 />
               </div>
-              <div className="listeBadge-listes">
-                <div style={{ height: 400, width: "100%" }}>
-                  <div className="listeBadge-title">
-                    <h2>Obtenus</h2>
-                  </div>
-                  <DataGrid
-                    rows={this.filtrerBadgesObtenus()}
-                    columns={columns}
-                  />
-                </div>
-                <div style={{ height: 400, width: "100%" }}>
+              <div style={{ height: 400, width: "100%" }}>
                 <div className="listeBadge-title">
-              <h2>Non obtenus</h2>
-            </div>
-                  <DataGrid
-                    rows={this.filtrerBadgesNonObtenus()}
-                    columns={columns}
-                  />
+                  <h2>Non obtenus</h2>
                 </div>
+                <DataGrid
+                  onRowClick={handleRowClick}
+                  rows={filtrerBadgesNonObtenus()}
+                  columns={columns}
+                />
               </div>
             </div>
+            <BadgePopup
+              isOpen={open}
+              selectedBadge={badgeSelectionne}
+              handleClose={handleClose}
+            />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default ListeBadge;
