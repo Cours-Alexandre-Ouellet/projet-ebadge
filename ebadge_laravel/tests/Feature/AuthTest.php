83a5aae9bd0b596fd05e628c8ae1f1d5a28b2912
pigeonsoldier <thomas.laliberte@icloud.com>
2025-05-16 20:23:07 -0400

    petite correction tests

--- ebadge_laravel/tests/Feature/AuthTest.php
+++ ebadge_laravel/tests/Feature/AuthTest.php
@@ -9,7 +9,6 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Laravel\Passport\Bridge\AccessToken;
 use Tests\TestCase;
 class AuthTest extends TestCase
